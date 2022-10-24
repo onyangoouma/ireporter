3a59e800e8a938d3668baa01ae66241eb398b242
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-10-24 19:20:47 +0300

    modifies sigup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,32 @@
 
 
 export default function Signup() {
+    //Registration states
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // //Submission states
+    // const [submitted, setSubmitted] = useState('');
+
+    // const handleName = (e)=> {
+    //     setName(e.target.value);
+    //     setSubmitted(false);
+    // }
+
+    // //Handle Submission
+    // const handleSubmit = (e) => {
+    //     e.preventDefaukt();
+    //     if (name === '' || email === ''|| password === '') {
+    //     SpeechSynthesisErrorEvent(true);
+    //     }else {
+    //         setSubmitted(true);
+    //         setError(false);
+    //     }
+    // };
+
   return (
-    <div>This is the Signup</div>
+    <div>
+      
+    </div>
   )
 }
