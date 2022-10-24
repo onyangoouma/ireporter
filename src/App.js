1e25f60491628c197a9c09de256405c0af0c272d
rickie <rickiemaweu@gmail.com>
2022-10-24 18:52:11 +0300

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,108 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/HOME/Navbar";
+import Slide from "./components/HOME/Slide";
+import Footer from "./components/HOME/Footer";
+import About from "./components/HOME/About";
+import Cases from "./components/HOME/Cases";
+import Partners from "./components/HOME/Partners";
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
+      <Navbar/>
+      <Slide/>
+      <About/>
+      <Partners/>
+      <Cases/>
+      <Footer/>
+      
     </div>
   );
-}const wrapper = document.querySelector(".sliderWrapper");
-const menuItems = document.querySelectorAll(".menuItem");
-
-const products = [
-  {
-    id: 1,
-    title: "History of Collapsed Buildings in Nairobi",
-
-  },
-  {
-    id: 2,
-    title: "About ",
-
-  },
-  {
-    id: 3,
-    title: "TESTIMONIALS",
-    
-  },
-  {
-    id: 4,
-    title: "Cases",
-   
-  },
- 
-]
-
-let choosenProduct = products[0];
-
-const currentProductImg = document.querySelector(".productImg");
-const currentProductTitle = document.querySelector(".productTitle");
-const currentProductPrice = document.querySelector(".productPrice");
-const currentProductColors = document.querySelectorAll(".color");
-const currentProductSizes = document.querySelectorAll(".size");
-
-menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    //change the current slide
-    wrapper.style.transform = `translateX(${-100 * index}vw)`;
-
-    //change the choosen product
-    choosenProduct = products[index];
-
-    //change texts of currentProduct
-    currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = "$" + choosenProduct.price;
-    currentProductImg.src = choosenProduct.colors[0].img;
-
-    //assing new colors
-    currentProductColors.forEach((color, index) => {
-      color.style.backgroundColor = choosenProduct.colors[index].code;
-    });
-  });
-});
-
-currentProductColors.forEach((color, index) => {
-  color.addEventListener("click", () => {
-    currentProductImg.src = choosenProduct.colors[index].img;
-  });
-});
-
-currentProductSizes.forEach((size, index) => {
-  size.addEventListener("click", () => {
-    currentProductSizes.forEach((size) => {
-      size.style.backgroundColor = "white";
-      size.style.color = "black";
-    });
-    size.style.backgroundColor = "black";
-    size.style.color = "white";
-  });
-});
-
-const productButton = document.querySelector(".productButton");
-const payment = document.querySelector(".payment");
-const close = document.querySelector(".close");
-
-productButton.addEventListener("click", () => {
-  payment.style.display = "flex";
-});
-
-close.addEventListener("click", () => {
-  payment.style.display = "none";
-});
-
+  
+}
 export default App;
